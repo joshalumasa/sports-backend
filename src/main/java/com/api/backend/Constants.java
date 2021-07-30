d76fc27dfd43e8b1b755e8a855a6b0b1b9bfdf05
Mogakamo <mogaka.amo254@gmail.com>
2021-07-30 10:17:33 +0300

    finished with the constants for the project

--- src/main/java/com/api/backend/Constants.java
+++ src/main/java/com/api/backend/Constants.java
@@ -1,4 +1,38 @@
 package com.api.backend;
 
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
 public class Constants {
+    String result = "";
+    InputStream inputStream;
+
+    public static String seasons_endpoint = "https://api-football-v1.p.rapidapi.com/v3/standings?season=2020&league=39";
+    public static String rapid_apiKey = "407f57c6c3msh39a5081824ca9eep1f6f52jsn6a9a6e410883";
+
+    public String getPropValues(String propertyKey) throws IOException {
+        try {
+            Properties prop = new Properties();
+            String propFileName = "config.properties";
+
+            inputStream = getClass().getClassLoader().getResourceAsStream(propFileName);
+
+            if (inputStream != null) {
+                prop.load(inputStream);
+            } else {
+                throw new FileNotFoundException("property file '" + propFileName + "' not found in the classpath. ");
+            }
+
+            //get the property value and print it out
+            result = prop.getProperty(propertyKey);
+        } catch (Exception e) {
+            System.out.println("Exception: " + e);
+        } finally {
+            assert inputStream != null;
+            inputStream.close();
+        }
+        return result;
+    }
 }
