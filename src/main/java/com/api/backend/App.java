2d0f4d147941e39b164622af2cbea241f125a351
Mogakamo <mogaka.amo254@gmail.com>
2021-07-30 09:31:40 +0300

    Initial commit

---
+++ src/main/java/com/api/backend/App.java
@@ -0,0 +1,4 @@
+package com.api.backend;
+
+public class App {
+}
---
+++ src/main/java/com/api/backend/Store.java
@@ -0,0 +1,52 @@
+package com.api.backend;
+
+import java.io.File;
+
+public class Store {
+    File dir = new File(File.separator+"home"+File.separator+"amos"+File.separator+"dev"+File.separator+"news-app");
+
+    public File getDir() {
+        createFpDir();
+        return dir;
+    }
+    public static String [] accountTypes;
+
+    public static String [] accountTypes_id;
+
+    public static String[] getAccountTypes() {
+        return accountTypes;
+    }
+
+    public static void setAccountTypes(String[] accountTypes) {
+        Store.accountTypes = accountTypes;
+    }
+
+    public static String[] getAccountTypes_id() {
+        return accountTypes_id;
+    }
+
+    public static void setAccountTypes_id(String[] accountTypes_id) {
+        Store.accountTypes_id = accountTypes_id;
+    }
+    private void createFpDir(){
+
+        if (!dir.exists()){
+            try {
+                // create dir
+                dir.mkdirs();
+
+                // give permissions
+                dir.setReadable(true, false);
+                dir.setWritable(true, false);
+                dir.setExecutable(true, false);
+
+            }
+            catch (Exception e){
+                System.out.println("An error in creating dir");
+                e.printStackTrace();
+            }
+        }
+        else
+            System.out.println("Dir exists");
+    }
+}
